e4526485bd35a9f0d953053d513699b67720b8be
Aman <amanchahar175@gmail.com>
2023-10-19 00:14:39 +0530

    some basic JS

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-console.log("AMAN")
+let age=21
+let Myname= "Aman"
+let isMale=true
+console.table([typeof(age),typeof(Myname),typeof(isMale)]);
+//type of function use hota hai kisi variable ya constant ka type bataane ke liye...
+//console.table([]) se jo print hona hai woh tabulat form mei hoga
